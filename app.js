47adb4b40cfc1af724f8c0693d827a67e0c15b72
Abhi Jain <abhijain9807@gmail.com>
2021-09-19 17:58:29 +0530

    banana talk clone completed

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log(125)
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json";
+
+// console.log(txtInput);
+function getTranslationURL(text){
+    return serverURL + "?" + "text" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time");
+}
+
+function clickHandler(){
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerHTML = translatedText;
+        })
+        .catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click", clickHandler);
