4e62d4f023440f63355bf575c37b66322e5b8676
Abhi Jain <abhijain9807@gmail.com>
2021-09-19 17:59:37 +0530

    cleaned code

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-
 var serverURL = "https://api.funtranslations.com/translate/valyrian.json";
 
-// console.log(txtInput);
 function getTranslationURL(text){
     return serverURL + "?" + "text" + text
 }
